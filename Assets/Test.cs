7dc4629b4f3fffda649b90c028c049ba1b27218b
devkdb <tengal73@naver.com>
2021-01-19 23:14:33 +0100

    # Play & Component 패턴 # # 유니티는 모든게 컴포넌트 패턴으로 만들어짐. 무조건 부품을 조립해서 만든 느낌. # Cube를 만들고 Inspector를 보면 뭔가 이런 저런 부품들이 붙어있다. # # 클라이언트 프로그램을 만드는 것은 영화 만드는 것과 같고, 서버는 레스토랑 경영하는 것과 같다. # # C# 파일명 변경시 수동으로 비쥬얼스튜디오에서도 바꿔줘야한다.

---
+++ Assets/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update <-- 맨 처음 딱 한번 실행.
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame  <- 프래임마다 계속 실행. 매번 틱마다 실행.
+    void Update()
+    {
+        transform.Rotate(new Vector3(1.0f, 1.0f, 1.0f));
+    }
+}
